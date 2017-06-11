e061b6cc3231907f6d3f73fe49a995e2166b0b6d
alinedoleron <doleron.aline@gmail.com>
2017-06-11 12:39:11 -0300

    Adding infos of Contact, Work and Projects

--- js/helper.js
+++ js/helper.js
@@ -15,7 +15,7 @@ replace the %data% placeholder text you see in them.
 var HTMLheaderName = '<h1 id="name">%data%</h1>';
 var HTMLheaderRole = '<span>%data%</span><hr>';
 
-var HTMLcontactGeneric = '<li class="flex-item"><span class="orange-text">%contact%</span><span class="white-text">%data%</span></li>';
+var HTMLcontactGeneric = '<li class="flex-item"><span class="orange-text">contact</span><span class="white-text">%data%</span></li>';
 var HTMLmobile = '<li class="flex-item"><span class="orange-text">mobile</span><span class="white-text">%data%</span></li>';
 var HTMLemail = '<li class="flex-item"><span class="orange-text">email</span><span class="white-text">%data%</span></li>';
 var HTMLtwitter = '<li class="flex-item"><span class="orange-text">twitter</span><span class="white-text">%data%</span></li>';
@@ -40,7 +40,7 @@ var HTMLprojectStart = '<div class="project-entry"></div>';
 var HTMLprojectTitle = '<a href="#">%data%</a>';
 var HTMLprojectDates = '<div class="date-text">%data%</div>';
 var HTMLprojectDescription = '<p><br>%data%</p>';
-var HTMLprojectImage = '<img src="%data%">';
+var HTMLprojectImage = '<img src="%data%" width="80px" length="80px">';
 
 var HTMLschoolStart = '<div class="education-entry"></div>';
 var HTMLschoolName = '<a href="#">%data%';
--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,111 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+var bio = {
+  name :  "Aline D'Oleron",
+  role : "Web developer",
+  contacts : {
+    mobile: "(81) 99283-8875",
+    email: "doleron.aline@gmail.com",
+     github: "alinedoleron",
+     location: "Recife"
+  },
+  welcomeMessage: "Welcome,",
+  skills : ["HTML", "CSS", "JavaScript","JQuery"],
+  biopic : "images/aline_bw.jpg"
+  //display: função,
+}
+
+var projects = {
+  projects: [{
+    title: "Projeto #1" ,
+    dates: "2017",
+    description: "Projeto a ser feito...",
+    images: ["images/em-construcao.png"]
+  }]
+  // display: função
+}
+
+var work = {
+  jobs: [
+  {
+    employer: "CIn-UFPE Projeto Motorola",
+    title: "Engenheira de Testes",
+    location: "Recife-PE",
+    dates: "Dez 2014-Jan 2017",
+    description: "Execução de testes manuais exploratórios, sanity e regression em aparelhos móveis."
+  },
+  {
+    employer: "Accenture",
+    title: "Engenheira de Software",
+    location: "Recife-PE",
+    dates: "Dez 2013-Dez 2014",
+    description: "Projeto Java Web com JSF. Participação na fase de desenvolvimento, deploy, testes e"+
+                  "correção de bugs."
+  },
+  {
+    employer: "LIKA Laboratório de Imunopatologia Keizo Asami/UFPE",
+    title: "Estagiária em Desenvolvimento Web",
+    location: "Recife-PE",
+    dates: "Set 2012 – Mai 2013",
+    description: "Trabalhei na criação de uma aplicação Web utilizando a linguagem Java com JSF,"+
+                " Hibernate e MySQL."
+  }]
+  //display: função
+}
+
+var formattedName = HTMLheaderName.replace("%data%", bio.name);
+var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
+var formattedMobile =  HTMLmobile.replace("%data%", bio.contacts["mobile"]);
+var formattedEmail =  HTMLemail.replace("%data%", bio.contacts["email"]);
+var formattedGithub =  HTMLgithub.replace("%data%", bio.contacts["github"]);
+var formattedLocation =  HTMLlocation.replace("%data%", bio.contacts["location"]);
+var formattedPictureBio =  HTMLbioPic.replace("%data%", bio.biopic);
+var formattedWelcomeMsg =  HTMLwelcomeMsg.replace("%data%", bio.welcomeMessage);
+
+
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
+$("#topContacts").append(formattedMobile);
+$("#topContacts").append(formattedEmail);
+$("#topContacts").append(formattedGithub);
+$("#topContacts").append(formattedLocation);
+$("#header").append(formattedPictureBio);
+$("#header").append(formattedWelcomeMsg);
+$("#header").append(HTMLskillsStart);
+
+(bio.skills).forEach(function (skill) {
+  var formattedSkills =  HTMLskills.replace("%data%", skill);
+  $("#skills").append(formattedSkills);
+});
+
+$("#workExperience").append(HTMLworkStart);
+
+(function (){
+  for (var i = 0; i < (work.jobs).length; i++) {
+    var employer = HTMLworkEmployer.replace("%data%",work.jobs[i]["employer"]);
+    var title = HTMLworkTitle.replace("%data%",work.jobs[i]["title"]);
+    var location = HTMLworkLocation.replace("%data%",work.jobs[i]["location"]);
+    var dates = HTMLworkDates.replace("%data%",work.jobs[i]["dates"]);
+    var description = HTMLworkDescription.replace("%data%",work.jobs[i]["description"]);
+    $("#workExperience .work-entry").append(employer);
+    $("#workExperience .work-entry").append(title);
+    $("#workExperience .work-entry").append(dates);
+    $("#workExperience .work-entry").append(location);
+    $("#workExperience .work-entry").append(description);
+  }
+})();
+
+$("#projects").append(HTMLprojectStart);
+for (var i = 0; i < projects.projects.length; i++) {
+  var title = HTMLprojectTitle.replace("%data%", projects.projects[i]["title"]);
+  var dates = HTMLprojectDates.replace("%data%", projects.projects[i]["dates"]);
+  var description = HTMLprojectDescription.replace("%data%", projects.projects[i]["description"]);
+
+  $("#projects .project-entry").append(title);
+  $("#projects .project-entry").append(dates);
+  $("#projects .project-entry").append(description);
+  projects.projects[i].images.forEach(function (img) {
+    var image = HTMLprojectImage.replace("%data%", img);
+    $("#projects .project-entry").append(image);
+  })
+
+
+}
